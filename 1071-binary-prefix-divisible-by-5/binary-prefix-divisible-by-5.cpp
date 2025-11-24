cafdd9a3a29b2727bc8316620a97a8b53593d9ce
nikhil6611 <nikhiltiwari6611@gmail.com>
2025-11-24 20:06:22 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (69.40%) - LeetSync

--- 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.cpp
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.cpp
@@ -5,7 +5,7 @@ public:
         int x=0;
         for(int i=0;i<nums.size();i++){
             x= ((x*2) +nums[i])%5;
-            if(x%5==0){
+            if(x==0){
                 answer[i]=true;
             }
             else{
