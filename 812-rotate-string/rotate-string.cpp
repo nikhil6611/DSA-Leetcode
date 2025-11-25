955945b65accf5cd890acd3c6b7d2bc664f29a76
nikhil6611 <nikhiltiwari6611@gmail.com>
2025-11-26 00:02:38 +0530

    Time: 2 ms (12.54%) | Memory: 8.3 MB (24.65%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,22 +1,50 @@
 class Solution {
 public:
+    bool kmp_match(string &t, string &goal){
+        vector<int>lps(goal.size(),0);
+        int pre=0, suff=1;
+        while(suff<goal.size()){
+            if(goal[pre]==goal[suff]){
+                lps[suff]= pre+1;
+                pre++;
+                suff++;
+            }
+            else if(pre==0){
+                lps[suff]=0;
+                suff++;
+            }
+            else{
+                pre = lps[pre-1];
+            }
+        }
+        int mainindex=0, subindex=0;
+        while(mainindex<t.size()){
+            if(t[mainindex]==goal[subindex]){
+                mainindex++;
+                subindex++;
+            }
+            else if(subindex==0){
+                mainindex++;
+            }
+            else{
+                subindex = lps[subindex-1];
+            }
+            if(subindex==goal.size()){
+                return true;
+            }
+        }
+        return false;
+    }
     bool rotateString(string s, string goal) {
         if(s==goal){
             return true;
         }
-       
-           
-        
-        for(int i=0;i<s.size();i++){
-           char c= s[0];
-            for(int i=1;i<s.size();i++){
-                s[i-1]=s[i];
-            }
-            s[s.size()-1]=c;
-           if(s==goal){
-            return true;
-           }
-        }
+       if(s.size()!=goal.size()){
         return false;
+       }
+       string t=s;
+       t+=s;
+       return kmp_match(t,goal);
+       
     }
 };
