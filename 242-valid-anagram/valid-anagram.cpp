35e40929283b3926a9806af256f3dcf4b6efc856
nikhil6611 <nikhiltiwari6611@gmail.com>
2025-11-25 23:45:38 +0530

    Time: 7 ms (20.50%) | Memory: 9.6 MB (73.52%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,16 +4,11 @@ public:
         if(s.size()!=t.size()){
             return false;
         }
-        int CHAR[256]={0};
-        for(int i =0;i<s.size();i++){
-            CHAR[s[i]]++;
-            CHAR[t[i]]--;
-        }
-        for(int i=0; i<256;i++){
-            if(CHAR[i]!=0){
-                return false;
-            }
-        }
+       sort(s.begin(), s.end());
+       sort(t.begin(),t.end());
+       if(s==t){
         return true;
+       }
+       return false;
     }
 };
